1ac3b448305dae787fd77668626197ddc35946b7
NickVatne <nicovatne@gmail.com>
2019-04-28 11:38:33 +0200

    Simple Map View to find stores near you

--- app/src/main/java/com/example/sneakeryard/screens/StoreFragment.kt
+++ app/src/main/java/com/example/sneakeryard/screens/StoreFragment.kt
@@ -7,29 +7,58 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.sneakeryard.R
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class StoreFragment : Fragment(), OnMapReadyCallback {
-    private lateinit var mMap: GoogleMap
 
-    override fun onMapReady(googleMap: GoogleMap?) {
-        mMap = googleMap!!
+    private lateinit var mGooglemap: GoogleMap
+    private lateinit var mMapView: MapView
+    private  lateinit var mView: View
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-         return inflater.inflate(R.layout.fragment_store, null)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mView = inflater.inflate(R.layout.fragment_store, container, false)
+        return mView
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mMapView = mView.findViewById<View>(R.id.map) as MapView
+        if (mMapView != null) {
+            mMapView!!.onCreate(null)
+            mMapView!!.onResume()
+            mMapView!!.getMapAsync(this)
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        MapsInitializer.initialize(context!!)
+
+        mGooglemap = googleMap
+        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        googleMap.addMarker(
+            MarkerOptions().position(
+                LatLng(
+                    59.911491,
+                    10.757933
+                )
+            ).title("Oslo").snippet("Sneakershops in Oslo")
+        )
+        val Oslo = CameraPosition.builder().target(LatLng(59.911491, 10.757933)).zoom(16f).bearing(0f).tilt(45f).build()
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Oslo))
+
+
+    }
 
 }
